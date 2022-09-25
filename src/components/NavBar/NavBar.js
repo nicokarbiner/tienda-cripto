53bc50a4ed1f58c3b0a6e3b33fb0451b5279c2a9
Nicolas Karbiner <nkarbiner@gmail.com>
2022-09-25 20:11:29 -0300

    NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,25 @@
-export const NavBar = () => {
+import "./navBar.scss";
+
+const NavBar = () => {
     return (
-        <div>
-            <p>Menu</p>
+        <div className="navbarContainer">
+            <h2>Tokenizar</h2>
+            <ul className="navbarMenu">
+                <li className="navbarItem">
+                    <a href="#">Home</a>
+                </li>
+                <li className="navbarItem dropdown">
+                    <a href="#">Categories</a>
+                </li>
+                <li className="navbarItem">
+                    <a href="#">Contact</a>
+                </li>
+                <li className="navbarItem">
+                    <a href="#">Cart</a>
+                </li>
+            </ul>
         </div>
-    )
-}
+    );
+};
+
+export default NavBar;
